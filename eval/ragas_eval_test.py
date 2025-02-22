7880fdb5a6e7f4aa3588829632bc59a53ad0fe00
Carlos <carlosgsouza@gmail.com>
2025-02-21 19:04:05 -0800

    Revert "Converting eval to a script so we can use CircleCI evaluation"
    This reverts commit ec9eb43759d157d999f8d1029652bb781bfac678.

--- eval/ragas_eval_test.py
+++ eval/ragas_eval_test.py
@@ -1,6 +1,5 @@
-import glob
 import json
-import os
+import pytest
 
 from langchain.docstore.document import Document
 from langchain_openai import ChatOpenAI
@@ -10,16 +9,38 @@ from ragas.llms import LangchainLLMWrapper
 from ragas.metrics import Faithfulness, LLMContextPrecisionWithReference, LLMContextRecall, ResponseRelevancy
 
 from continuous_rag_eval.rag import RAG
+import glob
 
-def run_evaluation():
-  eval_dataset = load_eval_dataset()
-  rag = initialize_rag(eval_dataset)
+@pytest.fixture
+def eval_dataset():
+  dataset = []
+  for filepath in glob.glob("eval/data/*.json"):
+    with open(filepath, "r") as f:
+      data = json.load(f)
+      dataset.extend(data)
+  yield dataset
 
+@pytest.fixture
+def rag(eval_dataset):
+  r = RAG()
+
+  documents = []
+  for q in eval_dataset:
+    for c in q["context"]:
+      doc = Document(
+          page_content = c["page_content"],
+          metadata = {"title": c["metadata"]["title"]}
+      )
+      documents.append(doc)
+  r.store_documents(documents)
+
+  return r
+
+def test_eval_rag(eval_dataset, rag):
   # Executes a query for each entry in the eval dataset and stores the input and output data in
   # samples, which is passed to Ragas for evaluation..
   samples = []
-  # TODO: Evaluate the entire dataset once it is ready.
-  for q in eval_dataset[:3]:
+  for q in eval_dataset:
     # Skips entries from the eval dataset that have just the context, but no questions.
     if "question" not in q:
       continue
@@ -49,43 +70,35 @@ def run_evaluation():
   ]
 
   # Performs the actual evaluation and uploads the results to app.ragas.io.
-  eval_result = evaluate(evaluation_dataset, metrics)
-  results_url = eval_result.upload()
-  print(f"Results dashboard: {results_url}")
+  try:
+    eval_result = evaluate(evaluation_dataset, metrics)
+    eval_result.upload()
+  except Exception as e:
+    print(f"Uploading results to Ragas failed: {e}")
 
-  # Exports the aggregated metrics to a JSON file.
+  # Validates the aggrregated score (calculated by validate_aggrregated_score) satisfies min_values.
+  validate_aggregated_score(
+    eval_result,
+    min_values={
+        ResponseRelevancy.name: 0.1,
+        Faithfulness.name: 0.1,
+        LLMContextPrecisionWithReference.name: 0.1,
+        LLMContextRecall.name: 0.1,
+    },
+  )
+
+def validate_aggregated_score(eval_result, min_values):
+  """
+  Validates that the aggregated scores for each metric meet the minimum values.
+  """
+  # The aggregated scores are the mean of the score for each indivual entry.
   scores = eval_result.to_pandas()
   aggregated_scores = {
-    metric_name: scores[metric_name].mean() for metric_name in [m.name for m in metrics]
+    metric_name: scores[metric_name].mean() for metric_name in min_values.keys()
   }
-  
-  os.makedirs("eval-results", exist_ok=True)
-  with open("eval-results/aggregated_metrics.json", "w") as f:
-    json.dump(aggregated_scores, f, indent=2)
 
-def load_eval_dataset():
-  dataset = []
-  for filepath in glob.glob("eval/data/*.json"):
-    with open(filepath, "r") as f:
-      data = json.load(f)
-      dataset.extend(data)
-  return dataset
+  # Creates a test for each metric defined in min_values.
+  print(f"Aggregated Scores: {aggregated_scores}")
 
-def initialize_rag(eval_dataset):
-  r = RAG()
-
-  documents = []
-  for q in eval_dataset:
-    for c in q["context"]:
-      doc = Document(
-          page_content = c["page_content"],
-          metadata = {"title": c["metadata"]["title"]}
-      )
-      documents.append(doc)
-  r.store_documents(documents)
-
-  return r
-
- 
 if __name__ == '__main__':
-  run_evaluation()
+  pytest.main()
